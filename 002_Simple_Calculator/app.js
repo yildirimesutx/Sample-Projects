5c22ff481869658e04e56ecf48b8d4c016d7cd0c
mesut <yildirimesutcw@gmail.com>
2022-02-15 00:52:26 +0300

    v2

---
+++ 002_Simple_Calculator/app.js
@@ -0,0 +1,12 @@
+let numberOne = document.getElementById("number_one");
+let newOperator = document.getElementById("operator");
+let numberTwo = document.getElementById("number_two");
+
+let answer = document.getElementById("calcu");
+
+
+
+
+
+
+
