f893ca86ee565b397209f03354c9ad2e86187870
mesut <yildirimesutcw@gmail.com>
2022-02-15 22:07:17 +0300

    the end

--- 002_Simple_Calculator/app.js
+++ 002_Simple_Calculator/app.js
@@ -2,9 +2,40 @@ let numberOne = document.getElementById("number_one");
 let newOperator = document.getElementById("operator");
 let numberTwo = document.getElementById("number_two");
 
-let answer = document.getElementById("calcu");
+let newButtun = document.getElementById("calcu");
+let newResult = document.getElementById("result");
 
 
+newButtun.addEventListener("click", ()=>{
+
+    let a = numberOne.value;
+    let b = numberTwo.value;
+    
+     if (newOperator.value == "+"){
+         let resulte = (+a) + (+b);
+         newResult.innerHTML = resulte
+     }
+     else if (newOperator.value == "-"){
+        let resulte = (+a) - (+b);
+        newResult.innerHTML = resulte
+     } 
+     else if (newOperator.value == "/"){
+        let resulte = (+a) / (+b);
+        newResult.innerHTML = resulte
+     } 
+     else if (newOperator.value == "*"){
+        let resulte = (+a)* (+b);
+        newResult.innerHTML = resulte
+     }
+
+    
+     else{
+        alert("invalid entry");
+    }
+
+    
+
+})
 
 
 
