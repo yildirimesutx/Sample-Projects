4296f304982be1965a5f48c118a833db0fc84c8b
mesut <yildirimesutcw@gmail.com>
2022-03-06 00:51:16 +0300

    digital clock

---
+++ 008_Digital Clock/app.js
@@ -0,0 +1,27 @@
+let newHour = document.querySelector(".hour");
+let newMinutes = document.querySelector(".minute")
+let newSecond = document.querySelector(".second")
+
+function clock(){
+    let d = new Date();
+    let h = d.getHours().toString().padStart(2,"0")
+    let m = d.getMinutes().toString().padStart(2,"0")
+    let s = d.getSeconds().toString().padStart(2,"0")
+
+    newHour.innerText= h 
+    newMinutes.innerText = m
+    newSecond.innerText = s
+}
+
+setInterval(clock,1000)
+
+
+
+
+
+
+
+
+
+
+
